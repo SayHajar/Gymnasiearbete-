935c33569d038ad1051b795d6a68644e29f64fd5
FelixR08 <felix.rothman@elev.ga.lbs.se>
2026-09-18 14:26:11 +0200

    push

---
+++ Gymnasietarbete/Assets/Scripts/Map.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+    public GameObject map;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+        }
+    }
+}
